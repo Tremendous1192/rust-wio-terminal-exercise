0aac989d471daf9b627f2ce42ca00dfaea624bb8
Tremendous1192 <tremendous.duck@gmail.com>
2023-07-21 21:21:12 +0900

    ファイル名の番号振りなおし

--- ch03_p42_01_hello_world/src/main.rs
+++ ch03_p42_01_hello_world/src/main.rs
@@ -9,40 +9,9 @@ const DISPLAY_HEIGHT: u32 = 240; // 画面高さの定数. ほぼ必須で良い
 #[wio_terminal::entry] // 必須アトリビュート
 fn main() -> ! {
     // 初期化
-    // ディスプレイドライバ(wio_terminal)
-    let core::prelude::v1::Ok((mut display, _backlight)) = preset_display() else { todo!() };
-
-    // 背景(embedded_graphics)
-    let background = preset_background(&mut display);
-
-    // Hello Worldの文字列
-    // フォント設定
-    use embedded_graphics::mono_font::MonoTextStyle;
-    let style = MonoTextStyle::new(
-        &embedded_graphics::mono_font::ascii::FONT_6X10,
-        embedded_graphics::pixelcolor::Rgb565::WHITE,
-    );
-    // 文字列の描画
-    let x: i32 = 10;
-    let y: i32 = (DISPLAY_HEIGHT / 2u32) as i32;
-    embedded_graphics::text::Text::new(
-        "Hello Rust!\nI am surprised that file size rose drastically.",
-        embedded_graphics::prelude::Point::new(x, y),
-        style,
-    )
-    .draw(&mut display)
-    .unwrap();
-
-    // 組込みはloop必須
-    loop {}
-    // ここまでloop処理
-}
-// ここまでmain関数
-
-/// Wio Terminalのディスプレイドライバを初期化する
-fn preset_display() -> Result<(wio_terminal::LCD, wio_terminal::aliases::LcdBacklight), ()> {
+    // 制御関係(wio_terminal)
     let mut peripherals = wio_terminal::pac::Peripherals::take().unwrap(); // 周辺機器(CPUコアとメモリ以外)
-    let core = wio_terminal::pac::CorePeripherals::take().unwrap(); // 周辺機器(CPUコアとメモリ以外)
+    let mut core = wio_terminal::pac::CorePeripherals::take().unwrap(); // 周辺機器(CPUコアとメモリ以外)
     let sets = wio_terminal::Pins::new(peripherals.PORT).split(); // 入出力
 
     // 画面表示の描画間隔を設定するために時計インスタンス
@@ -58,39 +27,53 @@ fn preset_display() -> Result<(wio_terminal::LCD, wio_terminal::aliases::LcdBack
 
     // ディスプレイドライバ
     use wio_terminal::prelude::_atsamd21_hal_time_U32Ext;
-    sets.display.init(
-        &mut clocks,
-        peripherals.SERCOM7,
-        &mut peripherals.MCLK,
-        100_u32.mhz(),
-        &mut delay,
-    )
-}
+    let (mut display, _backlight) = sets
+        .display
+        .init(
+            &mut clocks,
+            peripherals.SERCOM7,
+            &mut peripherals.MCLK,
+            10_u32.mhz(), // 書籍やChatGPTのままだと型名をつけましょうと怒られる
+            &mut delay,
+        )
+        .unwrap();
+    // ここまで 制御関係(wio_terminal)
 
-use embedded_graphics; // 描画関係
-use embedded_graphics::prelude::*; // 役割が多いのでとりあえずインポートしておく
-
-/// ディスプレイの背景を初期化する
-fn preset_background(
-    display: &mut wio_terminal::LCD,
-) -> embedded_graphics::primitives::Styled<
-    embedded_graphics::primitives::Rectangle,
-    embedded_graphics::primitives::PrimitiveStyle<embedded_graphics::pixelcolor::Rgb565>,
-> {
-    // フォント等の設定
+    // 描画関係(embedded_graphics)
+    // 描画設定
     let style = embedded_graphics::primitives::PrimitiveStyleBuilder::new()
         .fill_color(embedded_graphics::pixelcolor::Rgb565::BLACK)
         .build(); // 塗りつぶしの設定
-
-    // 背景は塗りつぶし長方形を用いる
+    // 背景の描画
     let background = embedded_graphics::primitives::Rectangle::new(
         embedded_graphics::prelude::Point::new(0, 0),
         embedded_graphics::prelude::Size::new(DISPLAY_WIDTH, DISPLAY_HEIGHT),
     )
-    .into_styled(style);
+    .into_styled(style); // 背景の設定
+    background.draw(&mut display).unwrap();
 
-    // 塗りつぶし処理
-    background.draw(display).unwrap();
+    use embedded_graphics::mono_font::MonoTextStyle;
+    let x: i32 = 10;
+    let y: i32 = (DISPLAY_HEIGHT / 2u32) as i32;
+    let style = MonoTextStyle::new(
+        &embedded_graphics::mono_font::ascii::FONT_6X10,
+        embedded_graphics::pixelcolor::Rgb565::WHITE,
+    );
+    embedded_graphics::text::Text::new(
+        "Hello Rust!\nI am surprised that file size rose drastically.",
+        embedded_graphics::prelude::Point::new(x, y),
+        style,
+    )
+    .draw(&mut display)
+    .unwrap();
+    // ここまで 描画関係(embedded_graphics)
 
-    background
+    // 組込みはloop必須
+    loop
+    {}
+    // ここまでloop処理
 }
+// ここまでmain関数
+
+use embedded_graphics; // 描画関係
+use embedded_graphics::prelude::*; // 役割が多いのでとりあえずインポートしておく
